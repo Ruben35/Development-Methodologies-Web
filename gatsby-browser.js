57150a9b803a1041b02fe9dbdf86cf64df0ba62f
Ruben35 <ruben111416@gmail.com>
2022-08-02 22:48:19 -0500

    💄 Adding Global Styles

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+// * Importing the font poppins
+import "@fontsource/poppins/800.css" //ExtraBold
+import "@fontsource/poppins/700.css" //Bold
+import "@fontsource/poppins/400.css" //Regular
+// * Importing Global CSS
+import "./src/styles/global.css"
